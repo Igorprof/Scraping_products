0c5dbb9c8f50046ecb426ceb67be14a2136f9fc0
Igor <gormal98@gmail.com>
2022-09-02 18:02:51 +0300

    added pipeline

--- product_scraper/pipelines.py
+++ product_scraper/pipelines.py
@@ -6,8 +6,40 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class ProductScraperPipeline:
+    def open_spider(self, spider):
+        self.connection = sqlite3.connect('db.db')
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS products(
+                title TEXT,
+                price TEXT,
+                description TEXT,
+                image TEXT
+            )
+        """)
+
+        self.connection.commit()
+
     def process_item(self, item, spider):
-        return item
+        self.cursor.execute("""
+            INSERT INTO products(
+                title,
+                price,
+                description,
+                image
+            ) VALUES (
+                ?,
+                ?,
+                ?,
+                ?
+            )
+        """, (item.get('title'), item.get('price'), item.get('description'), item.get('image')))
+
+        self.connection.commit()
+
+    def close_spider(self, spider):
+        self.connection.close()
